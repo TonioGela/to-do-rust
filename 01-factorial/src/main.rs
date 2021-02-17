4a237645498a5225a83c1021a0461e2f19deef85
Antonio Gelameris <toniogela89@gmail.com>
2021-02-17 11:04:26 +0100

    01-factorial + tests

--- 01-factorial/src/main.rs
+++ 01-factorial/src/main.rs
@@ -23,4 +23,26 @@ fn factorial(n: u128) -> u128 {
     value
 }
 
-//TODO add tests for factorial!
+#[cfg(test)]
+mod tests {
+    use super::factorial;
+    #[test]
+    pub fn factorial_should_work() {
+        test_factorial(1, 1);
+        test_factorial(2, 2);
+        test_factorial(3, 6);
+        test_factorial(4, 24);
+        test_factorial(5, 120);
+        test_factorial(6, 720)
+    }
+
+    fn test_factorial(n: u128, result: u128) {
+        assert_eq!(
+            factorial(n),
+            result,
+            "Factorial of {} was not {}",
+            n,
+            result
+        )
+    }
+}
